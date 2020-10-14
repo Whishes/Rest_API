389d375c13c0d9d4b9d8a87ca8ef40cbcda0f3d5
Whishes <nicholas.cruickshank25@gmail.com>
2020-10-15 10:57:44 +1100

    Quick fixes + testing due to time contraints
    # Added the body-parser package
    # Refactor routes for future expansion

--- index.js
+++ index.js
@@ -1,40 +1,17 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const routes = require('./routes/main');
+const passwordRoutes = require('./routes/password');
 const app = express();
 const port = 3000;
 
-app.get('/', (request, response) => {
-    console.log(request);
-    response.send('Hello world');
-});
+// Updating Express settings
+app.use(bodyParser.urlencoded({ extended: false})); // parse application/x-www-form-urlencoded
+app.use(bodyParser.json()); // parse application/json
 
-app.get('/status', (request, response) => {
-    response.status(200).json({message: 'Ok', status: 200 });
-});
-
-app.post('/signup', (request, response, next) => {
-    next(new Error('test'));
-    //response.status(200).json({message: 'Ok', status: 200 });
-});
-
-app.post('/login', (request, response) => {
-    response.status(200).json({message: 'Ok', status: 200 });
-});
-
-app.post('/logout', (request, response) => {
-    response.status(200).json({message: 'Ok', status: 200 });
-});
-
-app.post('/token', (request, response) => {
-    response.status(200).json({message: 'Ok', status: 200 });
-});
-
-app.post('/forgot-password', (request, response) => {
-    response.status(200).json({message: 'Ok', status: 200 });
-});
-
-app.post('/reset-password', (request, response) => {
-    response.status(200).json({message: 'Ok', status: 200 });
-});
+// Setting up other routes
+app.use('/', routes);
+app.use('/', passwordRoutes);
 
 // Catch all other routes
 app.use((request, response) => {
---
+++ routes/main.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (request, response) => {
+    console.log(request);
+    response.send('Hello world');
+});
+
+router.get('/status', (request, response) => {
+    response.status(200).json({message: 'Ok', status: 200 });
+});
+
+router.post('/signup', (request, response, next) => {
+    if (!request.body) {
+        response.status(400).json({message: 'invalid body', status: 400 });
+    } else {
+        response.status(200).json({message: 'Ok', status: 200 });
+    }
+});
+
+router.post('/login', (request, response) => {
+    if (!request.body) {
+        response.status(400).json({message: 'invalid body', status: 400 });
+    } else {
+        response.status(200).json({message: 'Ok', status: 200 });
+    }
+});
+
+router.post('/logout', (request, response) => {
+    if (!request.body) {
+        response.status(400).json({message: 'invalid body', status: 400 });
+    } else {
+        response.status(200).json({message: 'Ok', status: 200 });
+    }
+});
+
+router.post('/token', (request, response) => {
+    if (!request.body || !request.body.refreshToken) {
+        response.status(400).json({message: 'invalid body', status: 400 });
+    } else {
+        const { refreshToken } = request.body;
+        response.status(200).json({message: `refresh token requested for token: ${refreshToken}`, status: 200 });
+    }
+});
+
+module.exports = router;

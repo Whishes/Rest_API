9ff1023672ef8143d6c25b3a66d7ede44a99b824
Whishes <nicholas.cruickshank25@gmail.com>
2020-10-14 14:49:18 +1100

    Basic Routes added (quickly did it)
    # Setting up basic routes for all the ones that were planned out
    # Set up a 404 status & a 500 error message

--- index.js
+++ index.js
@@ -11,8 +11,40 @@ app.get('/status', (request, response) => {
     response.status(200).json({message: 'Ok', status: 200 });
 });
 
-app.post('/signup', (request, response) => {
+app.post('/signup', (request, response, next) => {
+    next(new Error('test'));
+    //response.status(200).json({message: 'Ok', status: 200 });
+});
+
+app.post('/login', (request, response) => {
     response.status(200).json({message: 'Ok', status: 200 });
+});
+
+app.post('/logout', (request, response) => {
+    response.status(200).json({message: 'Ok', status: 200 });
+});
+
+app.post('/token', (request, response) => {
+    response.status(200).json({message: 'Ok', status: 200 });
+});
+
+app.post('/forgot-password', (request, response) => {
+    response.status(200).json({message: 'Ok', status: 200 });
+});
+
+app.post('/reset-password', (request, response) => {
+    response.status(200).json({message: 'Ok', status: 200 });
+});
+
+// Catch all other routes
+app.use((request, response) => {
+    response.status(404).json({message: '404 - Not Found', status: 404});
+});
+
+// Handling Errors
+app.use((error, request, response, next) => {
+    console.log(error);
+    response.status(error.status || 500).json({error: error.message, status: 500});
 });
 
 app.listen(port, () => {
